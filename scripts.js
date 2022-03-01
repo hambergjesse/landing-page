932cb5d23fdc461d800c564de3de49834944d8c7
hambergjesse <hambergjesse@gmail.com>
2022-03-01 15:00:38 +0200

    idk

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//I'm so cool.
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
